a591d133eac826a2877cdf9d3f533dec7ef37c0c
neethupath <pathnee222gmail.com>
2023-03-16 20:51:37 -0400

    Key

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Your code here
+
+  var dodger = document.getElementById('dodger');
+
+dodger.style.backgroundColor = "#ff69b4";
+dodger.style.bottom = "100px";
+
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10);
+
+  if ( left > 0 ) {
+    dodger.style.left = `${left - 4}px`;
+  }
+}
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10);
+
+  if ( left < 360) {
+    dodger.style.left = `${left + 4}px`;
+  }
+}
+
+
+ document.addEventListener('keydown', function(e) {
+
+  if ( e.key === 39)
+{
+  moveDodgerRight();
+}
+  if(e.key === "ArrowRight"){
+    moveDodgerRight();
+  }
+});
